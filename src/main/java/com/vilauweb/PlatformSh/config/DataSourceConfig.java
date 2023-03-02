b9efd7909b9b287f47c700503eae87c9b67b467b
VilauGC <vilau.gc@gmail.com>
2023-03-02 13:26:11 +0200

    logging

--- src/main/java/com/vilauweb/PlatformSh/config/DataSourceConfig.java
+++ src/main/java/com/vilauweb/PlatformSh/config/DataSourceConfig.java
@@ -1,5 +1,7 @@
 package com.vilauweb.PlatformSh.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import sh.platform.config.Config;
 import sh.platform.config.MySQL;
@@ -7,12 +9,19 @@ import sh.platform.config.MySQL;
 import javax.sql.DataSource;
 
 @Configuration
+@Slf4j
 public class DataSourceConfig {
 
-    //@Bean(name="dataSource")
+    @Bean(name="dataSource")
     public DataSource getDataSource(){
         Config config = new Config();
         MySQL database = config.getCredential("database", MySQL::new);
+
+        log.info("**************** VGC-The path is: {}", database.getPath());
+        log.info("**************** VGC-The username is: {}", database.getUserName());
+        log.info("**************** VGC-The port is: {}", database.getPort());
+        log.info("**************** VGC-The host is: {}", database.getHost());
+
         return database.get();
     }
 
