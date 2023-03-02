db17fff3b9c7b28e13efa3ac75b6ad778060de12
VilauGC <vilau.gc@gmail.com>
2023-03-02 17:45:29 +0200

    testing without config

--- src/main/java/com/vilauweb/PlatformSh/config/DataSourceConfig.java
+++ src/main/java/com/vilauweb/PlatformSh/config/DataSourceConfig.java
@@ -5,14 +5,24 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Slf4j
+//@EnableTransactionManagement
 public class DataSourceConfig {
 
 //    @Bean(name = "dataSource")
 //    public DataSource getDataSource() {
-//        log.error("A INTRAT PE AICI!**********************************************");
-//        Config config = new Config();
-//        MySQL database = config.getCredential("db", MySQL::new);
-//        return database.get();
+//
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setUrl(url);
+//        dataSource.setUsername(username);
+//        dataSource.setPassword(password);
+//        dataSource.setDriverClassName(driverClassName);
+//
+//        return dataSource;
+//
+////        log.error("A INTRAT PE AICI!**********************************************");
+////        Config config = new Config();
+////        MySQL database = config.getCredential("database", MySQL::new);
+////        return database.get();
 //    }
 
 }
