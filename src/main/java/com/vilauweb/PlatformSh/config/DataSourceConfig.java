ee38c2ccbf91a0b3f57923ca9bb2a1f8e0e6d982
VilauGC <vilau.gc@gmail.com>
2023-03-02 16:53:54 +0200

    testing

--- src/main/java/com/vilauweb/PlatformSh/config/DataSourceConfig.java
+++ src/main/java/com/vilauweb/PlatformSh/config/DataSourceConfig.java
@@ -1,23 +1,18 @@
 package com.vilauweb.PlatformSh.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import sh.platform.config.Config;
-import sh.platform.config.MySQL;
-
-import javax.sql.DataSource;
 
 @Configuration
 @Slf4j
 public class DataSourceConfig {
 
-    @Bean(name = "dataSource")
-    public DataSource getDataSource() {
-        log.error("A INTRAT PE AICI!**********************************************");
-        Config config = new Config();
-        MySQL database = config.getCredential("db", MySQL::new);
-        return database.get();
-    }
+//    @Bean(name = "dataSource")
+//    public DataSource getDataSource() {
+//        log.error("A INTRAT PE AICI!**********************************************");
+//        Config config = new Config();
+//        MySQL database = config.getCredential("db", MySQL::new);
+//        return database.get();
+//    }
 
 }
